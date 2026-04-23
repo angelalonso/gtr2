cec3a1d59e90842d102eb007e3f178be84a11226
Angel Alonso Fonseca <angel.alonso.fonseca@westernacher.com>
2026-04-23 12:01:53 +0200

    tool to prepare zip files

--- dyn_ai/pack.sh
+++ dyn_ai/pack.sh
@@ -1,2 +1,2 @@
-rm -f dyn_ai_v1.0.0.z* && zip -s 49m -r dyn_ai_v1.0.2.zip dyn_ai.exe ai_data.db README.md datamgmt_dyn_ai.exe
-rm -f dyn_ai_v1.0.0_full.z* && zip -r dyn_ai_v1.0.2_full.zip dyn_ai.exe ai_data.db README.md datamgmt_dyn_ai.exe
+rm -f dyn_ai_v1.0.2.z* && zip -s 49m -r dyn_ai_v1.0.2.zip dyn_ai.exe ai_data.db README.md datamgmt_dyn_ai.exe
+rm -f dyn_ai_v1.0.2_full.z* && zip -r dyn_ai_v1.0.2_full.zip dyn_ai.exe ai_data.db README.md datamgmt_dyn_ai.exe
